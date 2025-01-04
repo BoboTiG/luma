d80eb770b6ce42f06aff781502001c49a48f1cce
Mickaël Schoentgen <contact@tiger-222.fr>
2025-01-04 16:12:28 +0100

    tweak: github-deploiement-vers-ex2

--- sources/dev/snippets/github-deploiement-vers-ex2.sh
+++ sources/dev/snippets/github-deploiement-vers-ex2.sh
@@ -7,6 +7,6 @@ FTP_USER='USERNAME'
 FTP_PASSWORD='PASSWORD'
 
 lftp \
-    -e "set ssl:verify-certificate false ; mirror --delete --transfer-all --reverse --verbose=1 ./FOLDER . ; quit" \
+    -e 'set ssl:verify-certificate false ; mirror --delete --transfer-all --reverse --verbose=1 ./FOLDER . ; quit' \
     -u "${FTP_USER},${FTP_PASSWORD}" \
     "ftp://${FTP_URL}"
